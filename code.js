0267e500fd5a533edca020db99fcc9a392d68301
Edgar_C <luncie.vii@gmail.com>
2023-02-16 12:46:51 -0600

    Starting proyect with dev.f specifications

---
+++ code.js
@@ -0,0 +1,30 @@
+const minBalance = 10;
+const maxBalance = 990;
+
+let cuentas = [
+    {nombre:'Mali', saldo:200},
+    {nombre:'Gera', saldo:290},
+    {nombre:'Maui', saldo:67},
+];
+
+// Añadir nuevos usuarios o cuentas
+// Eliminar cuentas pidiendo la contraseña (asegurate de retirar tu dinero o introduce una CLABE para transferir tu dinero a otra cuenta)
+// Si el password es incorrecto, notificar al usuario e intentarlo nuevamente
+
+    // $990 > cuenta > $10
+
+// Consultar Saldo
+// Ingresar Monto
+// Retirar Monto
+
+
+function createAccount(){};
+function deleteAccount(){};
+
+function insertPassword(){};
+function changePassword(){};
+
+
+function consultaSaldo(){};
+function ingresarSaldo(){};
+function retirarDinero(){};
