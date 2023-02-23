8536842753f0bd1a0510f0799ad84d189ef2e64e
luncied <luncie.vii@gmail.com>
2023-02-22 23:19:04 -0600

    correct the errors in js
    add an eventListener to link an action (submit) with a function (loginSession) and add and pointer event to capture the information that the function gives.

--- js/code.js
+++ js/code.js
@@ -1,5 +1,6 @@
-const minBalance = 10;
-const maxBalance = 990;
+// const minBalance = 10;
+// const maxBalance = 990;
+
 
 let cuentas = [
     {nombre:'Mali', pass:'1234', saldo:200},
@@ -8,20 +9,38 @@ let cuentas = [
 ];
 
 // pendiente
-let userName = document.getElementById('userName').value;
-let password = document.getElementById('pass').value;
+
+const formLogin = document.getElementById('login-form');
+const userName = document.getElementById('userName').value;
+const password = document.getElementById('pass').value;
+
+// formLogin.addEventListener('submit', function(e){
+//     e.preventDefault();
+// }); // Nos pide dos parametros, un evento y una funcion.
+// // "e" es un pointer event, captura información del evento que se ejecuta
 
 // && cuentas.find((pswrd) => pswrd.pass === password
 // Extraer unicamente el objeto del array "cuentas"
+
+// && 
+// cuentas.find((userPass) => userPass.pass == password)
+// cuentas.find((users) => users.nombre == userName)
+
+
 function loginSession(e){ 
     e.preventDefault();
-    if (cuentas.find((users) => users.nombre == userName) ){
-        alert('acceso correcto');
-    } else{
-        alert(pswrd);
-        alert(users);
-    };
+    console.log(e.target);
+    // if (cuentas.find((userPass) => userPass.pass == password)){
+    //     console.log(password);
+    //     // alert('acceso correcto');
+    // } else{
+    //     console.log(userName);
+    //     console.log(password);
+    // };
 };
+
+
+formLogin.addEventListener('submit', loginSession); // Nos pide dos parametros, un evento y una funcion.
 
 // function insertPassword(){};
 
