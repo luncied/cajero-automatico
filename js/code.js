d0d80c9129b1fd57a2b61261c8ebb50230ef115c
luncied <luncie.vii@gmail.com>
2023-02-22 19:06:32 -0600

    give format to the form with css
    move the format to the center and editing the navbar to have the title
    and the logo together and align. conecting the function with the
    button

--- js/code.js
+++ js/code.js
@@ -4,21 +4,22 @@ const maxBalance = 990;
 let cuentas = [
     {nombre:'Mali', pass:'1234', saldo:200},
     {nombre:'Gera', pass:'2345', saldo:290},
-    {nombre:'Maui', pass:'3456', saldo:67},
+    {nombre:'Maui', pass:'3456', saldo:67}
 ];
 
-
-let userName = document.getElementById('userName');
-let password = document.getElementById('pass');
+// pendiente
+let userName = document.getElementById('userName').value;
+let password = document.getElementById('pass').value;
 
 // && cuentas.find((pswrd) => pswrd.pass === password
 // Extraer unicamente el objeto del array "cuentas"
-function loginSession(){
+function loginSession(e){ 
+    e.preventDefault();
     if (cuentas.find((users) => users.nombre == userName) ){
         alert('acceso correcto');
     } else{
-        console.log(users);
-        console.log(pswrd);
+        alert(pswrd);
+        alert(users);
     };
 };
 
